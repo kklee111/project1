eb734c6456c38d03a0479177aca0bf2512f0d87e
kklee111 <khoiwoon.lee@gmail.com>
2018-10-23 10:21:30 +0530

    Update check_versions.py

--- check_versions.py
+++ check_versions.py
@@ -11,8 +11,8 @@ print('numpy: {}'.format(numpy.__version__))
 import matplotlib
 print('matplotlib: {}'.format(matplotlib.__version__))
 # pandas
-import pandas
-print('pandas: {}'.format(pandas.__version__))
+#import pandas
+#print('pandas: {}'.format(pandas.__version__))
 # scikit-learn
 import sklearn
 print('sklearn: {}'.format(sklearn.__version__))
