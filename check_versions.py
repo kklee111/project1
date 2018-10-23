109c91e4b3bd784569b5f6ad2bc9020f7a567385
kklee111 <khoiwoon.lee@gmail.com>
2018-10-23 10:17:49 +0530

    Update check_versions.py

---
+++ check_versions.py
@@ -0,0 +1,28 @@
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
